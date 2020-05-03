7ff7d7c40d09eb2e1a9b174bcae7a31c75092a7d
psbss <uypsbss@gmail.com>
2020-05-03 15:33:14 +0900

    add file

---
+++ 20200503/d-floor_function.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  long long A, B, N;
+  cin >> A >> B >> N;
+  long long x = min(B - 1, N);
+  int ans = (A * x) / B - A * (x / B);
+  cout << (A * x) / B - A * (x / B) << endl;
+}
